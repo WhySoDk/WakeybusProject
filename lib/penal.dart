8dc7830c2ab87e9f341dcb108e23ed0ba769c799
Copteez <copterkpd45@gmail.com>
2024-02-29 03:05:29 +0700

    Update stoplist make add stop list

--- lib/penal.dart
+++ lib/penal.dart
@@ -50,10 +50,75 @@ class _Penal extends State<Penal> {
                           context: context,
                           builder: (BuildContext context) {
                             return Container(
-                              height: 400,
+                              height: 360,
+                              width: 350,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(30),
                                 color: Colors.white,
+                              ),
+                              child:   Column(
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                                    child: TextField(
+                                      decoration: InputDecoration(
+                                          hintText: "Enter Stop name here"
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10),
+                                    child: Row(
+                                      children: [
+                                        const Expanded(
+                                            child: Text("Alarm Sound" ,style: TextStyle(fontSize: 16),)),
+                                        const SizedBox(
+                                          width: 150,
+                                        ),
+                                        Container(
+                                          height: 30,
+                                          width: 2,
+                                          color: Colors.grey,
+                                        ),
+                                        const Expanded(
+                                            child: naviSwitch())
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 1,
+                                    width: 300,
+                                    color: Colors.grey,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10),
+                                    child: Row(
+                                      children: [
+                                        const Expanded(
+                                            child: Text("Vibration",style: TextStyle(fontSize: 16),)),
+                                        const SizedBox(
+                                          width: 150,
+                                        ),
+                                        Container(
+                                          height: 30,
+                                          width: 2,
+                                          color: Colors.grey,
+                                        ),
+                                        const Expanded(
+                                            child: naviSwitch())
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 1,
+                                    width: 300,
+                                    color: Colors.grey,
+                                  ),
+                                  const Text("Radius"),
+                                  radiusRange(),
+                                  radiusRange(),
+                                  radiusRange(),
+                                ],
                               ),
                             );
                           });
@@ -146,6 +211,34 @@ class _naviSwitchState extends State<naviSwitch> {
       activeTrackColor: Color(0xFFB4D4FF),
       inactiveTrackColor: Colors.grey,
       inactiveThumbColor: Colors.white,
+    );
+  }
+}
+
+
+class radiusRange extends StatefulWidget {
+  radiusRange({super.key});
+
+  @override
+  State<radiusRange> createState() => _radiusRangeState();
+}
+
+class _radiusRangeState extends State<radiusRange> {
+  double _currentSlideValue  = 5;
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+        value: _currentSlideValue,
+        min: 1,
+        max: 10,
+        divisions: 10,
+        activeColor: Colors.blueAccent,
+        label: _currentSlideValue.round().toString(),
+        onChanged: (double value){
+          setState(() {
+            _currentSlideValue = value;
+          });
+        }
     );
   }
 }
